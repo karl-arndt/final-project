a5e61c85bed432ddb82b1dac90d15736f4e52046
karndt95 <karndt1995@gmail.com>
2024-04-07 11:15:36 -0400

    add functionality to addDrinkClicked function

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ function drinksClicked() {
   parent.innerHTML ="";
   for (const drink in DRINKS) {
     let child = document.createElement('div');
-    child.innerHTML = `<div class="drinks"><img src="${DRINKS[drink].img}" class="drink-img"><p>${DRINKS[drink].name}</p>
-      <p>$${Number(DRINKS[drink].price).toFixed(2)}</p><button id="${drink}"
-       onClick="addDrinkClicked(this.id)">Add</button></div>`;
+    child.innerHTML = `<div class="drinks"><img src="${DRINKS[drink].img}" class="drink-img">
+      <p>${DRINKS[drink].name}</p><p>$${Number(DRINKS[drink].price).toFixed(2)}</p>
+      <button id="${drink}" onClick="addDrinkClicked(this.id)">Add</button></div>`;
     parent.appendChild(child);
   }
 }
@@ -18,17 +18,29 @@ function sidesClicked() {
   parent.innerHTML ="";
   for (const side in SIDES) {
     let child = document.createElement('div');
-    child.innerHTML = `<div class="sides"><img src="${SIDES[side].img}" class="sides-img"><p>${SIDES[side].name}</p>
-      <p>$${Number(SIDES[side].price).toFixed(2)}</p><button id="${side}"
-       onClick="addSideClicked(this.id)">Add</button></div>`;
+    child.innerHTML = `<div class="sides"><img src="${SIDES[side].img}" class="sides-img">
+      <p>${SIDES[side].name}</p><p>$${Number(SIDES[side].price).toFixed(2)}</p>
+      <button id="${side}" onClick="addSideClicked(this.id)">Add</button></div>`;
     parent.appendChild(child);
   }
+}
+
+function addDrinkClicked(buttonID) {
+  if (cart.hasOwnProperty(buttonID)) {
+    let amount = Number(cart[buttonID].amount);
+    amount += 1;
+    cart[buttonID].amount = amount;
+  } else {
+    cart[buttonID] = {};
+    cart[buttonID].name = DRINKS[buttonID].name;
+    cart[buttonID].price = DRINKS[buttonID].price;
+    cart[buttonID].amount = 1;
+  }
+  alert(Object.entries(cart));
 }
 
 function addSideClicked(buttonID) {
   alert(buttonID);
 }
 
-function addDrinkClicked(buttonID) {
-  alert(buttonID);
-}
+

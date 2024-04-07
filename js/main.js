84350daaa4593994c6c8578ec20b9805f470600d
karndt95 <karndt1995@gmail.com>
2024-04-07 10:44:06 -0400

    Add onClick to add buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let language = 'en';
+let cart = {};
 
 function drinksClicked() {
   let parent = document.querySelector('#content');
@@ -6,7 +7,8 @@ function drinksClicked() {
   for (const drink in DRINKS) {
     let child = document.createElement('div');
     child.innerHTML = `<div class="drinks"><img src="${DRINKS[drink].img}" class="drink-img"><p>${DRINKS[drink].name}</p>
-      <p>$${Number(DRINKS[drink].price).toFixed(2)}</p><button id="${DRINKS[drink]}">Add</button></div>`;
+      <p>$${Number(DRINKS[drink].price).toFixed(2)}</p><button id="${drink}"
+       onClick="addDrinkClicked(this.id)">Add</button></div>`;
     parent.appendChild(child);
   }
 }
@@ -17,7 +19,16 @@ function sidesClicked() {
   for (const side in SIDES) {
     let child = document.createElement('div');
     child.innerHTML = `<div class="sides"><img src="${SIDES[side].img}" class="sides-img"><p>${SIDES[side].name}</p>
-      <p>$${Number(SIDES[side].price).toFixed(2)}</p><button id="${SIDES[side]}">Add</button></div>`;
+      <p>$${Number(SIDES[side].price).toFixed(2)}</p><button id="${side}"
+       onClick="addSideClicked(this.id)">Add</button></div>`;
     parent.appendChild(child);
   }
+}
+
+function addSideClicked(buttonID) {
+  alert(buttonID);
+}
+
+function addDrinkClicked(buttonID) {
+  alert(buttonID);
 }

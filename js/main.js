c4ff1b5aee9d006c212bd4dc00a3da051e0406a7
karndt95 <karndt1995@gmail.com>
2024-04-07 10:34:05 -0400

    Add id to add drink button

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function drinksClicked() {
   for (const drink in DRINKS) {
     let child = document.createElement('div');
     child.innerHTML = `<div class="drinks"><img src="${DRINKS[drink].img}" class="drink-img"><p>${DRINKS[drink].name}</p>
-      <p>$${Number(DRINKS[drink].price).toFixed(2)}</p><button>Add</button></div>`;
+      <p>$${Number(DRINKS[drink].price).toFixed(2)}</p><button id="${DRINKS[drink]}">Add</button></div>`;
     parent.appendChild(child);
   }
 }
@@ -17,7 +17,7 @@ function sidesClicked() {
   for (const side in SIDES) {
     let child = document.createElement('div');
     child.innerHTML = `<div class="sides"><img src="${SIDES[side].img}" class="sides-img"><p>${SIDES[side].name}</p>
-      <p>$${Number(SIDES[side].price).toFixed(2)}</p><button>Add</button></div>`;
+      <p>$${Number(SIDES[side].price).toFixed(2)}</p><button id="${SIDES[side]}">Add</button></div>`;
     parent.appendChild(child);
   }
 }

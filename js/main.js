200f8572f601cf9a26cbbe8e070426c3272ea859
karndt95 <karndt1995@gmail.com>
2024-04-07 08:51:16 -0400

    Add correct css class to sides

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function sidesClicked() {
   parent.innerHTML ="";
   for (const side in SIDES) {
     let child = document.createElement('div');
-    child.innerHTML = `<div class="drinks"><img src="${SIDES[side].img}" class="drink-img"><p>${SIDES[side].name}</p>
+    child.innerHTML = `<div class="sides"><img src="${SIDES[side].img}" class="sides-img"><p>${SIDES[side].name}</p>
       <p>$${Number(SIDES[side].price).toFixed(2)}</p><button>Add</button></div>`;
     parent.appendChild(child);
   }

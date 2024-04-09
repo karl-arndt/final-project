465c658141a31874f8646ed411068168330f17e9
karndt95 <karndt1995@gmail.com>
2024-04-09 11:18:10 -0400

    Add toppings of ramen to cart

--- js/main.js
+++ js/main.js
@@ -20,14 +20,14 @@ function ramenClicked() {
   parent.appendChild(form);
   for (const ramen in RAMEN) {
     let child = document.createElement('div');
-    child.innerHTML = `<input type="radio" id="${ramen}" name="ramen" value="${RAMEN[ramen].name}"
-       onclick="ramenRadioClicked(this.id)" checked>
+    child.innerHTML = `<input type="radio" id="${ramen}" name="ramen" value="${ramen}"
+      onclick="ramenRadioClicked(this.id)" checked>
       <label for="${ramen}">${RAMEN[ramen].name}</label>`;
     form.appendChild(child);
   }
   for (const topping in TOPPINGS) {
     let child = document.createElement('div');
-    child.innerHTML = `<input type="checkbox" id="${topping}" name="${topping}">
+    child.innerHTML = `<input type="checkbox" id="${topping}" name="topping">
       <label for="${topping}">${TOPPINGS[topping].name}</label>`;
     form.appendChild(child);
   }
@@ -38,7 +38,7 @@ function ramenClicked() {
 
 function sidesClicked() {
   let parent = document.querySelector('#content');
-  parent.innerHTML ="";
+  parent.innerHTML = "";
   for (const side in SIDES) {
     let child = document.createElement('div');
     child.innerHTML = `<div class="sides"><img src="${SIDES[side].img}" class="sides-img">
@@ -50,21 +50,36 @@ function sidesClicked() {
 
 function cartClicked() {
   let parent = document.querySelector('#content');
-  parent.innerHTML ='';
+  parent.innerHTML = '';
   for (const item in cart) {
     let child = document.createElement('div');
     child.innerHTML = `<div class="cart"><img src="${cart[item].img}" class="cart-img">
       <p>${cart[item].name}</p><p>x${cart[item].amount}</p>
       <p>$${(Number(cart[item].price) * Number(cart[item].amount)).toFixed(2)}</p></div>`
     parent.appendChild(child);
+
+    if (RAMEN.hasOwnProperty(item)) {
+      let grandchild = document.createElement('div');
+      for (index = 1; true; index += 1) {
+        if (!cart[item].hasOwnProperty(String(index))) {
+          break;
+        } else {
+          let arr = cart[item][String(index)];
+          for (let i = 0; i < arr.length; i++) {
+            grandchild.innerHTML += `<div class="cart-ramen-toppings">${TOPPINGS[arr[i]].name}</div>`;
+          }
+          child.appendChild(grandchild);
+        }
+      }
+    }
   }
 }
 
 function addDrinkClicked(buttonID) {
   if (cart.hasOwnProperty(buttonID)) {
-    let amount = Number(cart[buttonID].amount);
-    amount += 1;
-    cart[buttonID].amount = amount;
+    let count = Number(cart[buttonID].amount);
+    count += 1;
+    cart[buttonID].amount = count;
   } else {
     cart[buttonID] = {};
     cart[buttonID].img = DRINKS[buttonID].img;
@@ -76,9 +91,9 @@ function addDrinkClicked(buttonID) {
 
 function addSideClicked(buttonID) {
   if (cart.hasOwnProperty(buttonID)) {
-    let amount = Number(cart[buttonID].amount);
-    amount += 1;
-    cart[buttonID].amount = amount;
+    let count = Number(cart[buttonID].amount);
+    count += 1;
+    cart[buttonID].amount = count;
   } else {
     cart[buttonID] = {};
     cart[buttonID].img = SIDES[buttonID].img;
@@ -88,12 +103,44 @@ function addSideClicked(buttonID) {
   }
 }
 
+function ramenRadioClicked(radioID) {
+
+}
+
 function ramenSubmitClicked() {
-  alert('test');
+  // https://www.geeksforgeeks.org/how-to-get-value-of-selected-radio-button-using-javascript/
+  let ramenRadio = document.getElementsByName('ramen');
+  let newRamen;
+  for (let index = 0; index < ramenRadio.length; index += 1) {
+    if (ramenRadio[index].checked) {
+      newRamen = ramenRadio[index].id;
+      if (cart.hasOwnProperty(newRamen)) {
+        let count = Number(cart[newRamen].amount);
+        count += 1;
+        cart[newRamen].amount = count;
+        cart[newRamen][count] = ramenToppings();
+      } else {
+        cart[newRamen] = {};
+        cart[newRamen].img = RAMEN[newRamen].img;
+        cart[newRamen].name = RAMEN[newRamen].name;
+        cart[newRamen].price = RAMEN[newRamen].price;
+        cart[newRamen]["1"] = ramenToppings();
+        cart[newRamen].amount = 1;
+      }
+    }
+  }
 }
 
-function ramenRadioClicked(radio) {
-  alert(radio);
-}
 
+
+function ramenToppings() {
+  let toppingsCheckbox = document.getElementsByName('topping');
+  let toppings = [];
+  for (let index = 0; index < toppingsCheckbox.length; index += 1) {
+    if (toppingsCheckbox[index].checked) {
+      toppings.push(toppingsCheckbox[index].id);
+    }
+  }
+  return toppings;
+}
 
